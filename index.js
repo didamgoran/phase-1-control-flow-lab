cf5bb38fd45c14b03d7db8d0b6c194095d51dd86
Didam Goran <didamgoran01@gmail.com>
2024-04-14 01:14:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-function scuberGreetingForFeet(){
+
+
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400){
+      return  'This one is on me!';
+  } else if(someValue > 400 && someValue < 2000){
+    return 'That will be twenty bucks.';
+  } else if (someValue > 2000 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else if (someValue > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
+
+console.log(ternaryCheckCity("NYC"));
+console.log(ternaryCheckCity("Pittsburgh"));
+
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+
+  
 }
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+
+console.log(switchOnCharmFromTip('generous'));       
+console.log(switchOnCharmFromTip('not as generous')); 
+console.log(switchOnCharmFromTip('average'));         
+
